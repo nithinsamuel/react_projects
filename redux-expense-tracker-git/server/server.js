0687a312deeacabd0b9bddaca4697d0ee764b65a
nithinsam77 <nithinsam77@gmail.com>
2020-08-30 13:05:51 +0530

    updated with por no for prod build

--- redux-expense-tracker-git/server/server.js
+++ redux-expense-tracker-git/server/server.js
@@ -5,6 +5,8 @@ const app=express();
 // .. - we go up a folder
 // public is the file name with prod build
 const publicPath=path.join(__dirname,'..','public');
+const port=process.env.PORT||3000;
+
 app.use(express.static(publicPath));
 //app.get runs whenever a get request is made to our server
 //we can use * to match all unmatched routes
@@ -12,7 +14,7 @@ app.use(express.static(publicPath));
 app.get("*",(req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'))
 });
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("publicPath",publicPath)
     console.log('server is up')
 });
