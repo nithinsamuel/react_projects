b2d85b535031bf2bfc9f13bc245c15e301aab47d
nithinsam77 <nithinsam77@gmail.com>
2020-11-15 22:46:20 +0530

    initial commit

---
+++ youtube-api-redux-git/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+const API_KEY = "AIzaSyD-MDRPVdqsrFTcsagg9VuulKHcJN9Q1b0";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
