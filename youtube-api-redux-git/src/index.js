b8e182e15184fe8cdf514446ef53440288c004c6
nithinsam77 <nithinsam77@gmail.com>
2020-11-16 19:52:05 +0530

    moved the api key to config folder in gitigmore

--- youtube-api-redux-git/src/index.js
+++ youtube-api-redux-git/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-const API_KEY = "AIzaSyD-MDRPVdqsrFTcsagg9VuulKHcJN9Q1b0";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
